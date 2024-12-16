1b14a6cea69a484a67af30160115785ef228b744
janepanov <panovjane@gmail.com>
2024-12-16 09:47:10 +0100

    Fix bug in pop() method in Queue class

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -33,4 +33,11 @@ class QueueTest extends TestCase{
         $this->assertEquals(0, $this->queue->getCount());
         $this->assertEquals('green', $item);
     }
+
+    public function testAnItemIsRemovedFromTheFrontOfTheQueue(){
+        $this->queue->push('first');
+        $this->queue->push('second');
+
+        $this->assertEquals('first', $this->queue->pop());
+    }
 }
